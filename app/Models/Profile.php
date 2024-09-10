899bd6f005363cbe396cabc7d60e45e6ffdb5eb1
EdsonJunior <edsonjos61@gmail.com>
2024-09-10 18:35:59 -0300

    Organizing request return

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -20,6 +20,17 @@ class Profile extends Authenticatable
         'name'
     ];
 
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
     public function users()
     {
         return $this->belongsToMany(User::class, 'profiles_users', 'users_id', 'profiles_id');
---
+++ tests/Feature/Http/Controllers/UserControllerTest.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Response;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+# php artisan test --filter=UserControllerTest
+class UserControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->seed('ProfilesAndUsersSeeder');
+    }
+
+    public function authenticateUser()
+    {
+        $adminUser = User::where('email', 'admin@example.com')->first();
+
+        $response = $this->postJson(env('APP_URL').'/api/login', [
+            'email' => $adminUser->email,
+            'password' => 'Teste2@145',
+        ]);
+
+        return $response->json('data.token');
+    }
+
+    # php artisan test --filter=UserControllerTest::test_register_user_with_success
+    public function test_register_user_with_success()
+    {
+        $token = $this->authenticateUser();
+
+        $response = $this->withHeaders([
+                'Authorization' => 'Bearer ' . $token
+            ])->postJson(env('APP_URL').'/api/users', [
+                "name" => "Edson Junior",
+                "email" => "edsonjos61@gmail.com",
+                "password" => "Teste2@145",
+                "password_confirmation" => "Teste2@145",
+                "profile_name" => "Admin"
+            ]
+        );
+        
+        $response->assertStatus(Response::HTTP_CREATED)
+            ->assertJsonStructure([
+                'message',
+                'data' => [
+                    'User' => [
+                        'name',
+                        'email',
+                        'updated_at',
+                        'created_at',
+                        'id',
+                    ],
+                ],
+            ]
+        );
+    }
+}
